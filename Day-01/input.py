fcdbd385a8f6a2fa62e6b30a1df8ffbea367430b
Danish <faishaldanish177@gmail.com>
2026-07-26 20:18:50 +0530

    Create input.py

---
+++ Day-01/input.py
@@ -0,0 +1,12 @@
+#================================================
+#Topic:Input in Python
+#================================================
+#1. input() always returns a string
+#2. Using the same variable name again and again then the first value will be overwritten
+
+
+name=int(input("enter the username")) # input will be 
+print("Hello",name)
+
+height=float(input(" kitna lamba h tu?"))
+
