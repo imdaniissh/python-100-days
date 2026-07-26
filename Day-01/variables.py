b854e779a03c909039a7ccf3128d6d48bee2ac34
Danish <faishaldanish177@gmail.com>
2026-07-26 19:47:29 +0530

    Update variables.py

--- Day-01/variables.py
+++ Day-01/variables.py
@@ -1,1 +1,30 @@
+#==============================================
+# Variables in Python
+#==============================================
+# A variable is a named container that is used to store data in a memory.
+# It allows us to save some values and use them later in program
+
+# Why do we use variables?
+#1 To store Data
+#2 To reuse values
+#3 to make code readable and maintanable
+
+#Rules for naming variable:
+#1 Variable must start with letter and underscore(_).
+#2 It cannot start with a number.
+#3 spaces are not allowed
+#4 Variabes name are case sensitive
+#5 Avoiding python keywords for e.g-while,if for
+
+
+
+
+
+#syntax:-
+#variable_name = value
+name="dan" #string
+is_unemployed=False #boolean
+package=18.5 #float
+work_experience=1 #int
+
 
