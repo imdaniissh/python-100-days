2793c5da72a2863fc42d27d981063d7988a6221f
Danish <faishaldanish177@gmail.com>
2026-07-27 21:15:12 +0530

    Create operators.py

---
+++ Day-02/operators.py
@@ -0,0 +1,22 @@
+#
+==============================================
+# Operators in Python
+==============================================
+
+#Definition
+#Operators are special symbol used to perform operations on variables and values
+
+#Types of operators
+#1. Arithematic Operator (+,-,*,/,//,%,*8)
+#2. Assignment Operator (=,+=,-=,*=,/=,//=)
+#3.Comparison Operator (==,!=,>,<,>=,<=)
+#4.Logical Operator (and , or ,not)
+#5.Identity Operator (is, is not)
+#6.Membership Operator (in, not in)
+#7.Bitwise Operators(&,|,^,~,<<,>>)
+print(a==b)
+
+x=5
+y=4
+print(x>y)
+print(x!=y)
