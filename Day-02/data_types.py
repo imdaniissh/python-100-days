6f9942b9317e9466c3c56c7264464051e3a3a5f3
Danish <faishaldanish177@gmail.com>
2026-07-27 20:49:47 +0530

    Update data_types.py

--- Day-02/data_types.py
+++ Day-02/data_types.py
@@ -1,1 +1,18 @@
+#
+============================================
+#Data Types in Python
+#
+=============================================
 
+# Data types define the type of value that a variable can store.
+# Python automatically assigns a data type based on the value.
+
+# Basic Data Types:
+#1. int -> Stores whole numbers (e.g, 10,-5)
+#2. float -> Stores decimal numbers (e.g, 3.14,7.5)
+#3. str -> Stores text or chracters (e.g, "hello")
+#4. bool -> Stores Boolean values (True or False)
+
+age=23
+is_student=False
+name="check my resume for that"
