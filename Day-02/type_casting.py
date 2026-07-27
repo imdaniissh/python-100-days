37b9c2003ca60dda39783708fa82f66828c218f5
Danish <faishaldanish177@gmail.com>
2026-07-27 21:03:22 +0530

    Create type_casting.py

---
+++ Day-02/type_casting.py
@@ -0,0 +1,28 @@
+#
+=========================================
+# Type catsing in python
+#
+=========================================
+
+#Type casting is the process of converting one data type into another
+
+#Types of  Type Casting:
+#1. Implicit Type Casting(Automatic)
+#2. Explicit Type Casting(Manual)
+
+#1. Implicit Type Casting
+#Python automatically convert one data type into another when it needed.
+#e.g
+a=10
+b=2.5
+result = a + b
+print(result)
+print(type(result))
+
+#2.Explicit Type Casting
+#Explicit Type casting is the manual conversion of one data type into another
+
+#int()
+num="25"
+print(int(num))
+#same as for float str and bool()
