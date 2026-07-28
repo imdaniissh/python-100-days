82e7bb6565f4d660c95b82382af3a98ac9f98cf4
Danish <faishaldanish177@gmail.com>
2026-07-28 21:06:44 +0530

    Create if_elif_else.py

---
+++ Day-03/if_elif_else.py
@@ -0,0 +1,12 @@
+#
+=====================================
+#IF-ELIF-ELSE STATEMENT
+=====================================
+
+#Syntax
+if condition1:
+  staement
+elif condition2:
+  statement
+else:
+  statement
