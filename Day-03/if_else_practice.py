9e468c8904f1ef98ad54c081565cf3eb677edb2f
Danish <faishaldanish177@gmail.com>
2026-07-28 21:22:52 +0530

    Update if_else_practice.py

--- Day-03/if_else_practice.py
+++ Day-03/if_else_practice.py
@@ -18,7 +18,16 @@ if x%2==0:
       print("even h")
 else:
       print("odd h")
-      
 
-      
+#question 3 
+#Largest of two number
+x=int(input("enter the first number:")
+y=int(input("enter the second number:")
 
+if x>y:
+      print("first is greater")
+elif y>x:
+      print("second is greater")
+else:
+      print("equal")
+      
