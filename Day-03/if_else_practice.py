2ace96cf7c4e2ae31f814a99280faa62effeaf76
Danish <faishaldanish177@gmail.com>
2026-07-28 21:19:39 +0530

    Create if_else_practice.py

---
+++ Day-03/if_else_practice.py
@@ -0,0 +1,24 @@
+#question 1
+# Check +ve,-ve or zero
+x=int(input("enter the number:")
+if x>0:
+      
+      print("positive number")
+      
+elif x<0:
+
+      print("negative number")
+else:
+      print("zero")
+
+#question 2
+# check even or odd
+x=int(input("enter the number:")
+if x%2==0:
+      print("even h")
+else:
+      print("odd h")
+      
+
+      
+
