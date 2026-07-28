5f3572a8fd27559aa4baec0f53903a1a11afcb31
Danish <faishaldanish177@gmail.com>
2026-07-28 21:03:49 +0530

    Create if_else_and_nested_if.py

---
+++ Day-03/if_else_and_nested_if.py
@@ -0,0 +1,24 @@
+==============================
+# IF -ELSE Statement
+#
+==============================
+ #Definition
+# if-else statemnt is used to execute one block of code when the condition is True and another block of code when condition is False
+#Syntax 
+if condition:
+  #Statement
+
+else:
+  #Statement
+# Indentation:
+#Indentation means leaving space at the beginning of line
+#By convention, 4 spaces are use for each indentation level
+
+#Example
+if age>=18:
+  primt("Ready go to college")
+else:
+  print("pdh le")
+
+
+
