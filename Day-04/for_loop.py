8e34d62457ba3ab7cff5f16ea59da78bfb59ccdf
Danish <faishaldanish177@gmail.com>
2026-07-29 23:39:02 +0530

    Update for_loop.py

--- Day-04/for_loop.py
+++ Day-04/for_loop.py
@@ -9,4 +9,17 @@
 #Syntax
 for variable in sequence:
   #code to execute
+#range() function
+#the range() function generates a sequence of numbers 
+#syntax
+#range(stop)->Starting value(default is 0)
+#range(start,stop)
+#range(start,stop,step)
+#step-> Increment or decrement value(default is 1)
+#eg
+for i in range(5):
+  print(i)
+for i in range(2,11,2):
+  print(i)
+  
 
