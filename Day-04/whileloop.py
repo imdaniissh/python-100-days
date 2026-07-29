49720df4d7ab6ab9c241a747be600ed870120d0f
Danish <faishaldanish177@gmail.com>
2026-07-29 23:50:44 +0530

    Update whileloop.py

--- Day-04/whileloop.py
+++ Day-04/whileloop.py
@@ -1,1 +1,16 @@
+#
+==========================================
+#While loop in Python
+==========================================
+
+#Definition
+#A while loop is used to execute a block of code repeatedly as long as condition is true
+#syntax
+while condition:
+  #Code to execute
+#eg
+a=1
+while a<15:
+  print("github")
+  a+=1
 
