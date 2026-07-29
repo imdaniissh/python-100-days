31d61e4d2c99709a1f6bca032ea04064ae93bf2f
Danish <faishaldanish177@gmail.com>
2026-07-29 19:31:20 +0530

    Create for_loop.py

---
+++ Day-04/for_loop.py
@@ -0,0 +1,12 @@
+#
+=======================================
+# For Loop in Python
+#
+=======================================
+# Defination
+#A for loop is used to iterate over a sequence or to execute a block of code a specific number of time
+
+#Syntax
+for variable in sequence:
+  #code to execute
+
