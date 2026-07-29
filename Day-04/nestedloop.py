9a0cecee0021da1f186e674237b589d8412bf749
Danish <faishaldanish177@gmail.com>
2026-07-29 23:46:32 +0530

    Update nestedloop.py

--- Day-04/nestedloop.py
+++ Day-04/nestedloop.py
@@ -1,1 +1,15 @@
+#
+==============================================
+#Nested For Lop
+#
+==============================================
+#Definition
+#A nested loop is a loop inside another loop
+#The inner loop executes completely for each iteration of the outer loop
 
+#syntax
+for outer_variable in sequence:
+  for inner_variable in sequence:
+    #code to execute
+
+  
