4df2e03336360d7e7c5065dc1ad919344bfe7216
Danish <faishaldanish177@gmail.com>
2026-07-30 20:51:40 +0530

    Create infinteloop.py

---
+++ Day-05/infinteloop.py
@@ -0,0 +1,15 @@
+#
+================================
+#infinite loop
+================================
+#Definition
+#An infinite loop is a loop that keeps running continuosly because its stopping condition never become false
+#example
+while True:
+  print("hello")
+
+#Real life uses 
+#ATM Machine
+#Games
+#Chat Application
+
