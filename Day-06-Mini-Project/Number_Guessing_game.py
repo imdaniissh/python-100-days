b7721ab8f5113d6bca093758b03aab665a74015a
Danish <faishaldanish177@gmail.com>
2026-08-01 01:22:08 +0530

    Update and rename Mini-PROJECT.py to Number_Guessing_game.py

---
+++ Day-06-Mini-Project/Number_Guessing_game.py
@@ -0,0 +1,42 @@
+import random
+target=random.randint(1,100)
+attempt_count=0
+while attempt_count<10:
+  number=int(input("enter the number")
+  
+  difference=abs(target-number)
+  if number==target:
+             print("you have guessed the right number")
+             attempt_count+=1
+             print("Attempts:",attempt_count)
+             score=100-(attempt_count-1)*10
+             print("Score:",score)
+             break
+  elif number>100 or number<1:
+            print("invalid number")
+            
+            
+            continue
+     
+            
+  elif number>target and difference>=5:
+             print("too high")
+             attempt_count+=1
+             print("Attempt Left:",10-attempt_count)
+             
+             
+
+  elif number<target and difference>=5:
+             print("too low")
+             attempt_count+=1
+             print("Attempt Left:",10-attempt_count)
+             
+
+  else:
+            print("very close")
+            attempt_count+=1
+            print("Attempt Left:",10-attempt_count)
+
+if attempt_count==10:
+  print("Game over")
+  print("The correct number was",target)
