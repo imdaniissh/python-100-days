1509df3d6c1c36bd944303c85c7a95106a5a6934
Danish <faishaldanish177@gmail.com>
2026-08-01 21:04:35 +0530

    Update arguments.py

--- Day-07-Functions/arguments.py
+++ Day-07-Functions/arguments.py
@@ -1,1 +1,28 @@
+#===================================
+#FUNCTION ARGUMENTS
+#===================================
+
+#Arguments are values passed to a functiom
+
+#1.Positional Argument
+
+def introduce(name,age):
+  print("Name",name)
+  print("Age",age)
+introduce("Danish",23)
+
+#2.Keyword Argument
+
+def student(name,course):
+  print("Name:",name)
+  print("Course:",course)
+student(course="python",name="danish")
+
+#3. Default Arguments
+
+def country(name,country="India"):
+  print(name,"is from",country)
+  country("Danish")
+  country("John","USA")
+
 
