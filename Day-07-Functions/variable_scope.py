1d2c543eae97d6fa0d657f9ad4101a24cd2004fd
Danish <faishaldanish177@gmail.com>
2026-08-01 21:39:09 +0530

    Update variable_scope.py

--- Day-07-Functions/variable_scope.py
+++ Day-07-Functions/variable_scope.py
@@ -1,1 +1,22 @@
+#======================================
+# VARIABLE SCOPE
+#======================================
+
+# Scope determines where a variablecan be accessed.
+
+# 1.Local Variable
+def student():
+  name="Danish" # Local Variable:> Created inside a function, Can only be used inside that function
+  print("Inside Function:",name)
+student()
+
+#2. Global Variable
+college="JMI" #Global Variable:> created outside a function, Can be accessed from anywhere in the program
+
+def show_college():
+  print("College:",college)
+show_college()
+print("outside function:",college)
+
+
 
