b673069972a90258bf746fd3c1444c3736abc6f3
Danish <faishaldanish177@gmail.com>
2026-08-02 22:42:45 +0530

    Update practice_questions.py

--- Day-07-Functions/practice_questions.py
+++ Day-07-Functions/practice_questions.py
@@ -7,4 +7,47 @@ x=int(input("enter the number"))
 solution=square(x)
 print(solution)
       
-#2. Wr
+#2. Write a function to find the greatest of two number
+
+def greatest(a,b):
+  if a>b:
+    return a
+  else:
+    return b
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+solution=greatest(x,y)
+print(solution)
+
+#3. write a function to check even or odd number
+
+def is_even(num):
+  if num%2==0:
+    return "even"
+  else:
+    return "odd"
+num=int(input("enter the number")
+print(is_even(num))
+
+#4. write a fuction to make a calculator
+
+    def calculator(num1,operator,num2):
+      if operator== "+":
+        return num1+num2
+      elif operator== "-":
+        return num1-num2
+      elif operator== "*":
+       return num1*num2
+      elif operator="/":
+          return num1/num2
+      else:
+        return "invalid operator"
+
+      num1=int(input("enter the number")
+      num2=int(input("enter the number")
+      operator=input("enter the operator")
+      print(calculator(num1,operator,num2)
+            
+               
+
+    
