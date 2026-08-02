326540dfaacec27b577cb225404262b3ea9ebaec
Danish <faishaldanish177@gmail.com>
2026-08-02 22:12:32 +0530

    Update practice_questions.py

--- Day-07-Functions/practice_questions.py
+++ Day-07-Functions/practice_questions.py
@@ -1,1 +1,10 @@
+#1. Write a function square(number) that returns the square of a number
 
+def square(num):
+  ans=num*num
+  return ans
+x=int(input("enter the number"))
+solution=square(x)
+print(solution)
+      
+#2. Wr
