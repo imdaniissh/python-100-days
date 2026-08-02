faa03b9555a931b67a0aaa7665dca7d24c4a18d8
Danish <faishaldanish177@gmail.com>
2026-08-02 23:10:33 +0530

    Create list.py

---
+++ Day-08-List/list.py
@@ -0,0 +1,28 @@
+#=====================================
+LISTS IN PYTHON
+#=====================================
+
+# A list is an ordered and mutable collection used to store multiple items in a single variable
+
+#=====================================
+#Creating Lists
+#=====================================
+
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits)
+
+numbers= [10,20,30,40]
+print(numbers)
+
+mixed=["banana",10,3.5]
+
+empty_list=[]
+print(empty_list)
+
+
+#==========================
+#Length of List
+#==========================
+
+print(len(fruits)) #3
+
